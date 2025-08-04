64c3c9bb69aa169e1e3d1423da1c04a2371aa4bb
Omar Ben-khalif L. Disimban <obdisimban31286@liceo.edu.ph>
2025-08-04 15:57:42 +0800

    just finished

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Test Branch");
+        Book book1 = new Book("The Night Circus", "Erin Morgenstern", 387);
+        Book book2 = new Book("The Road", "Cormac McCarthy", 287);
+        Book book3 = new Book("The Midnight Library", "Matt Haig", 304);
+
+        performBookActions(book1);
+        performBookActions(book2);
+        performBookActions(book3);
+    }
+
+    public static void performBookActions(Book book) {
+        System.out.println("\n===== " + book.getTitle() + " =====");
+        book.displayInfo();
+        book.borrowBook();
+        book.displayInfo();
+        book.returnBook();
+        book.displayInfo();
     }
 }
