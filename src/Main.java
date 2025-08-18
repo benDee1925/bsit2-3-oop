8cc92544141cde6d589149b30a827b6eec17bb35
unknown <obdisimban312856@liceo.edu.ph.com>
2025-08-18 15:38:39 +0800

    lab-activity-4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Test Branch");
+        GradeCalculator calculator = new GradeCalculator();
+
+        String studentName = "John Smith";
+        double[] grades = {85.5, 92.0, 78.5, 90.0};
+
+
+        double average = calculator.calculateAverage(grades);
+
+
+        String letterGrade = calculator.getLetterGrade(average);
+
+        calculator.displayResult(studentName, average);
+        System.out.println();
+        calculator.displayResult(studentName, average, letterGrade);
     }
 }
