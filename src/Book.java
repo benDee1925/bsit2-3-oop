7335083860bc21bada693522958a94986924c904
Ben Dee <obdisimban31286@liceo.edu.ph>
2025-09-26 13:57:23 -1200

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,1 +1,37 @@
+class Book extends Media {
+    private String author;
 
+    public Book(String title, String itemId, String author) {
+        super(title, itemId);
+        this.author = author;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "Book";
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("[Book] Title: " + title + ", Author: " + author + ", ID: " + itemId + ", Available: " + isAvailable);
+    }
+}
+
+class DVD extends Media {
+    private String director;
+
+    public DVD(String title, String itemId, String director) {
+        super(title, itemId);
+        this.director = director;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "DVD";
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("[DVD] Title: " + title + ", Director: " + director + ", ID: " + itemId + ", Available: " + isAvailable);
+    }
+}
