fd5263f5a8bf2acbbbb4f4188e1a35d12a82ac4c
Ben Dee <obdisimban31286@liceo.edu.ph>
2025-10-02 15:54:59 +0800

    Update LibraryItem.java

--- src/LibraryItem.java
+++ src/LibraryItem.java
@@ -1,30 +1,28 @@
-abstract class LibraryItem {
+abstract class LibraryItem implements Borrowable {
     protected String itemId;
     protected String title;
     protected String author;
-    protected boolean isCheckedOut;
-    protected String borrowerName;
+    protected boolean isCheckedOut = false;
+    protected String borrowerName = "";
 
     public LibraryItem(String itemId, String title, String author) {
         this.itemId = itemId;
         this.title = title;
         this.author = author;
-        this.isCheckedOut = false;
-        this.borrowerName = null;
     }
 
     public String getItemInfo() {
-        return getItemType() + ": " + title + " by " + author;
+        return title + " (" + getItemType() + " - " + getBorrowingStatus() + ")";
     }
 
     public void checkOut(String borrowerName) {
-        this.isCheckedOut = true;
+        isCheckedOut = true;
         this.borrowerName = borrowerName;
     }
 
     public void checkIn() {
-        this.isCheckedOut = false;
-        this.borrowerName = null;
+        isCheckedOut = false;
+        this.borrowerName = "";
     }
 
     public abstract String getItemType();
