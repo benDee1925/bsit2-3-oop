e92e91f6ac8ad947c6d0cf9c8a8018aa7dd8061f
Ben Dee <obdisimban31286@liceo.edu.ph>
2025-10-02 15:19:07 +0800

    Update LibraryItem.java

--- src/LibraryItem.java
+++ src/LibraryItem.java
@@ -1,10 +1,10 @@
-public abstract class LibraryItem {
+abstract class LibraryItem {
     protected String itemId;
     protected String title;
     protected String author;
     protected boolean isCheckedOut;
     protected String borrowerName;
-    
+
     public LibraryItem(String itemId, String title, String author) {
         this.itemId = itemId;
         this.title = title;
@@ -12,23 +12,21 @@ public abstract class LibraryItem {
         this.isCheckedOut = false;
         this.borrowerName = null;
     }
-    
+
     public String getItemInfo() {
-        return String.format("%s [ID: %s] by %s", title, itemId, author);
+        return getItemType() + ": " + title + " by " + author;
     }
-    
+
     public void checkOut(String borrowerName) {
         this.isCheckedOut = true;
         this.borrowerName = borrowerName;
     }
-    
+
     public void checkIn() {
         this.isCheckedOut = false;
         this.borrowerName = null;
     }
-    
+
     public abstract String getItemType();
-    
     public abstract double calculateLateFee(int daysLate);
 }
-
